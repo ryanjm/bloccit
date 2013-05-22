f8baa7ad9e62a8859e41df91cf1490a2683f08ac
Ryan <ryan@ryanjm.com>
2013-05-22 12:37:53 -0600

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlocReddit::Application.config.secret_token = '1bb9854e6de4bc98f6630a8d902fa253fef0b3373e3eeab008a79f88030711440c89f1d411d8fd6fdef64854612c491abc2b06ec6ab4120dc8ae1a71d3fd4b08'
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+BlocReddit::Application.routes.draw do
+  get "welcome/index"
+
+  get "welcome/about"
+
+  root to: 'welcome#index'
+end
